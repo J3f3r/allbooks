96874ddce4864cdf8d0397832a7784c828d0930e
Jeferson de Souza Martins PCD <jsmartins959@gmail.com>
2025-10-13 19:58:31 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.crt')
 }, server).listen(8000, () => {
-  console.log("Acesse o endereço https://localhost:8000")
+  console.log("Acesse endereço https://localhost:8000")
 })
 
 /*server.listen(8000, () => {
