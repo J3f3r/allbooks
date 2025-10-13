c020aea24f4cc4c680a341c7db23a91c419f9ae5
Jeferson de Souza Martins PCD <jsmartins959@gmail.com>
2025-10-13 19:51:02 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.crt')
 }, server).listen(8000, () => {
-  console.log("Acesse a API em https://localhost:8000")
+  console.log("Acesse o endereço https://localhost:8000")
 })
 
 /*server.listen(8000, () => {
